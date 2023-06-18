9aa3630f569360279148d819864f76df440a9fb7
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-06-18 13:11:56 +0200

    Refactory all D3.js logic
    This is a large commit. I've moved all D3 code that updates DOM elements
    into separate functions. In doing so I have moved some code around,
    refactored it and changed som things. It's a lot to sum up so I'll let
    the code and comments speak for itself.
    
    A noteworthy change is the academic year footer. I've changed how it
    looks when as user isn't logged in, which also applies to block
    schedules that the user doesn't own. Before the buttons still shown but
    without text which made them look clickable. The hover effect also still
    applied. Now the buttons have been replaced by a wide bar of similar
    look that spans the entire academic year.

--- rodatraden/static/rodatraden/rodatraden_blocks_interface.js
+++ rodatraden/static/rodatraden/rodatraden_blocks_interface.js
@@ -1,8 +1,8 @@
 // Global data whose lifetime persists. This is needed since these variables
 // are used in callbacks.
 const scriptDataset = document.currentScript.dataset;
-let courses = getAllCourses();
-const coursesByYear = assignPositionsAndGroupByYear(courses);
+let course = getAllCourses();
+const coursesByYear = assignPositionsAndGroupByYear(course);
 
 /**
  * Setups event listeners for the buttons to update and delete the block
@@ -33,17 +33,17 @@ function setupSections() {
   
   // Setup onclick event listeners to show and hide the three different sections.
   $(".show-block").click(function() {
-    $("#study-block").show("slow");
+    $("#block-schedule").show("slow");
     $("#chart").hide("fast");
     $("#ISP").hide("fast");
   });
   $(".show-chart").click(function() {
-    $("#study-block").hide("fast");
+    $("#block-schedule").hide("fast");
     $("#chart").show("slow");
     $("#ISP").hide("fast");
   });
   $(".show-ISP").click(function() {
-    $("#study-block").hide("fast");
+    $("#block-schedule").hide("fast");
     $("#chart").hide("fast");
     $("#ISP").show("slow");
   });
@@ -59,7 +59,7 @@ function setupAddYearButton() {
   const button = document.getElementById('block-addyear');
   button.addEventListener('click', () => {
     addCourseYear(coursesByYear);
-    renderBlock();
+    renderBlockSchedule();
   });
 }
 
@@ -171,84 +171,88 @@ function getAllCourses() {
 }
 
 /**
-  * Renders study-block from the global course data.
-  * If the data changes you can just call this function again
-  * and the study-block will be updated accordingly with fancy
-  * animation representing the action taken.
-  */
-function renderBlock() {
-  const isLoggedIn = scriptDataset.isLoggedIn === 'True';
-
-  const xMax = 40;
-  const margin = 1;
-  const scale = 3;
-  const transitionDuration = 500;
-
-  // Make sure to sort the course groups by year in ascending order.
-  coursesByYear.sort((a, b) => a.year - b.year);
-
-  // Contains the rows of academic years. Each year has titles, courses and
-  // buttons.
-  const blockYears = d3.select('#block-years');
-
-  /* Start academic year */
-
-  //Data join - update selection
-  let academicYears = blockYears.selectAll(".academic-year")
+ * Adds, if needed, academic years divs using D3. These years holds headers,
+ * courses and buttons. Returns any newly created academic years.
+ * 
+ * @param {*} blockYears - D3 object of any newly created academic years.
+ * @param {number} transitionDuration - Transition duration in milliseconds.
+ * @returns D3 object containing any newly created academic years.
+ */
+function updateAcademicYear(blockYears, transitionDuration) {
+  // Create a selection of academic years, including those that need to be
+  // created, based on the data attached.
+  let academicYearSelection = blockYears.selectAll(".academic-year")
     .data(coursesByYear, courseGroup => courseGroup.year);
 
-  //ENTER
-  //Only added object logic here
-
-  // Add new academic year.
-  let academicYear = academicYears.enter()
-    .append("div")
+  // Add, if needed, new academic years.
+  let academicYear = academicYearSelection.enter().append("div")
     .attr("class", "academic-year text-center")
     .attr("id", courseGroup => courseGroup.year)
     .style("opacity", 1e-6);
+  
+  // Remove, if needed, old academic years.
+  academicYearSelection.exit()
+    .transition()
+    .duration(transitionDuration/4)
+    .style("opacity", 1e-6)
+    .remove();
 
   // Animate transition for new academic year.
-  academicYear.transition()
+  academicYear
+    .transition()
     .duration(transitionDuration)
     .style("opacity", 1);
 
-  /* End academic year */
+  return academicYear;
+}
 
-  /* End term header */
-
-  // Add a header to the academic year that shows the two terms.
-  let termsHeader = academicYear
-    .append("div")
+/**
+ * Adds, if needed, headers for each term of the academic years.
+ * 
+ * @param {*} academicYear - D3 data structure.
+ */
+function updateTermsHeader(academicYear) {
+  // Add a container for the term headers to any newly created academic years.
+  let termHeaderContainer = academicYear.append("div")
     .attr("class", "academic-year-header academic-year-header__terms");
-
-  termsHeader.selectAll(".academic-year-header__term")
+  
+  // Create a selection of term headers, including those that need to be
+  // created, based on the data attached.
+  let termHeaderSelection = termHeaderContainer.selectAll(".academic-year-header__term")
     .data(courseGroup => [
       { termPrefix: "HT", year: courseGroup.year },
       { termPrefix: "VT", year: courseGroup.year + 1 }
-    ])
-    .enter()
-    .append("div")
+    ]);
+    
+  // Add, if needed, headers to the academic years that shows the two terms.
+  termHeaderSelection.enter().append("div")
     .attr("class", "academic-year-header__term bg-dark")
     .text(term => term.termPrefix + term.year.toString().substr(-2));
+}
 
-  /* End term header */
-  
-  /* Start period header */
-
-  // Add a header to the academic year that shows the four periods.
-  let periodsHeader = academicYear
-    .append("div")
+/**
+ * Adds, if needed, headers for each period of the academic years.
+ * 
+ * @param {*} academicYear - D3 data structure.
+ */
+function updatePeriodsHeader(academicYear) {
+  // Add a header that shows the four periods to any newly created academic
+  // years.
+  let periodHeaderContainer = academicYear.append("div")
     .attr("class", "academic-year-header academic-year-header__periods");
 
-  periodsHeader.selectAll(".academic-year-header__period")
+  // Create a selection of period headers, including those that need to be
+  // created, based on the data attached.
+  let periodHeaderSelection = periodHeaderContainer.selectAll(".academic-year-header__period")
     .data(courseGroup => [
       { periodNumber: 1, year: courseGroup.year },
       { periodNumber: 2, year: courseGroup.year },
       { periodNumber: 3, year: courseGroup.year },
       { periodNumber: 4, year: courseGroup.year }
-    ])
-    .enter()
-    .append("div")
+    ]);
+  
+  // Add, if needed, headers to the academic years that shows the four periods.
+  periodHeaderSelection.enter().append("div")
     .attr("class", "academic-year-header__period bg-dark")
     .text(period => {
       const roundToOneDecimal = number => Math.round(number * 10) / 10;
@@ -262,124 +266,217 @@ function renderBlock() {
 
       return Math.round(studyPace * 100) + " % / " + roundToOneDecimal(ects) + " hp";
     });
+}
 
-    /* End period header */
+/**
+ * Adds, if needed, a container for all course blocks to the academic years.
+ * Returns any newly created course containers.
+ * 
+ * @param {*} academicYear - D3 data structure.
+ * @param {number} scale - Scale used when calculating the container height.
+ * @param {number} margin - Margin used when calculating the container height.
+ * @returns D3 object containing any newly created course containers.
+ */
+function updateCourseContainer(academicYear, scale, margin) {
+  // Add a container for all course blocks to any newly created academic years.
+  let courseContainer = academicYear.append("div")
+    .attr("class", "course-container")
+    .style("height", courseGroup => {
+      const height = courseBlockHeight(courseGroup.courses, scale, margin);
+      return height + "px";
+    });
+  
+  return courseContainer;
+}
 
-  //Block which holds the courses
-  var years = academicYear
-    .append("div")
-    .attr("class", "courses")
-    .style("height", function(d){
-      var height = courseBlockHeight(d.courses, scale, margin);
+/**
+ * Adds any new courses to the academic year. The courses include a title and a
+ * button to remove the course.
+ * 
+ * @param {*} academicYear - D3 data structure.
+ * @param {boolean} isLoggedIn - True if the user is logged in.
+ * @param {number} xMax - Distance used to calculating size and position.
+ * @param {number} scale - Scale used to calculating size and position.
+ * @param {number} margin - Margin used to calculating size and position.
+ */
+function updateCourseBlocks(academicYear, isLoggedIn, xMax, scale, margin) {
+  // Add a container for all course blocks to any newly created academic years.
+  let courseContainer = academicYear.append("div")
+    .attr("class", "course-container")
+    .style("height", courseGroup => {
+      const height = courseBlockHeight(courseGroup.courses, scale, margin);
       return height + "px";
     });
 
-  /* Footer that holds the smaller divs for adding new courses */
-  var footer = academicYear.append("div").attr("class", "block-footer");
+  // Create a selection of courses, including those that need to be created,
+  // based on the data attached.
+  let courseSelection = courseContainer.selectAll(".course")
+    .data(courseGroup => courseGroup.courses);
 
-  /* Add divs for footer buttons "add course" */
-  var footerLp = footer.selectAll(".block-footer-lp")
-    .data(function(d){
-      return [
-        {lp:1, year:d.year},
-        {lp:2, year:d.year},
-        {lp:3, year:d.year},
-        {lp:4, year:d.year}
-      ];
+  // Add, if needed, courses to the container.
+  // XXX: Many magic numbers are used to style the block.
+  let course = courseSelection.enter().append("div")
+    .attr('class', "course")
+    .style("height", course => {
+      const height = course.speed * scale - margin * 5;
+      return height + "px";
     })
-    .enter()
-    .append("div")
-    .attr("class", "block-footer-lp bg-dark btn add-course");
-
-  /* Append courses to block */
-  courses = years
-    .selectAll(".course")
-    .data(function(d){
-      return d.courses;
+    .style("width", course => {
+      const width = course.length / xMax * 100 - 0.5;
+      return width + "%";
     })
-    .enter()
-    .append("div")
-    .attr('class', function(d){
-      return "course";
-      /* TODO: Implement this */
-      //if (d.type) return "course course-" + d.type;
-      //var sometimesItIsHardToThinkOfAFittingVariableName = doIHavePrerequisites(coursesData, d.course.prerequisites, d.year, d.start);
-      //return "course prereq-" + sometimesItIsHardToThinkOfAFittingVariableName;
-    });
-
-  /* Position of course on the block */
-  courses
-    .style("height", function(d) {
-      var barHeight = (d.speed * scale) - margin*5;
-      return barHeight + "px";
-    })
-    .style("width", function(d) {
-      var barWidth = d.length / xMax * 100 - 0.5 ;
-      return barWidth + "%";
-    })
-    .style("margin-left", function(d) {
-      var left = d.start / xMax * 100 + 0.25;
+    .style("margin-left", course => {
+      const left = course.start / xMax * 100 + 0.25;
       return left + "%";
     })
-    .style("margin-top", function(d) {
-      var top = (d.firstRowIndex * scale) + margin*3;
+    .style("margin-top", course => {
+      const top = course.firstRowIndex * scale + margin * 3;
       return top + "px";
     });
 
-  /* Link to each course */
-  var courseLinks = courses.append("p")
+  // Add a title to the course.
+  // XXX: Why is the cursor set twice? Is it for backwards compatibility?
+  let courseTitle = course.append("p")
     .attr("class", "pr-4 pl-1 text-left")
-    .style("font-weight", "bold").style("cursor", "pointer")
-    .style("cursor", "hand").style("margin-top", "10px")
-    .append("a").text(function(d) {
-      return d.title;
-    });
+    .style("margin-top", "10px")
+    .style("font-weight", "bold")
+    .style("cursor", "pointer")
+    .style("cursor", "hand");
 
-  courseLinks
+  // The title includes a link that leads to information about the course.
+  courseTitle.append("a")
+    .text(course => {
+      return course.title;
+    })
     .attr('class', 'courseoccasion-info')
-    .attr('data-id', function(d) {
-      return scriptDataset.courseoccasionInfoUrl + "?year=" + d.year +
-        "&slug=" + d.slug;
+    .attr('data-id', course => {
+      const url = scriptDataset.courseoccasionInfoUrl + "?year=" + course.year
+        + "&slug=" + course.slug;
+      return url;
     });
 
-  academicYears.exit()
-    .transition()
-    .duration(transitionDuration/4)
-    .style("opacity", 1e-6)
-    .remove();
-
-
-  /* This stuff is not required if not logged in */
+  // Only logged in users can remove courses.
   if (isLoggedIn) {
-
-    /* Buttons to add a course */
-    footerLp
-      .text("Lägg till kurs")
-      .style("color", "white")
-      .attr("data-toggle", "tooltip")
-      .attr("data-placement", "left")
-      .attr('data-id', function(d) {
-        var start = (d.lp - 1)*10;
-        return scriptDataset.blockCourseListUrl + "?year=" + d.year + "&start=" + start;
+    // Add, if needed, a button to remove the course.
+    let removeButton = course.append("p")
+      .attr("class", "btn btn-link delete-course block-remove-button");
+    
+    // Setup data for the button.
+    // XXX: The page is refreshed each time a course is deleted. Could this be
+    // solved by instead sending a POST request?
+    removeButton.append("a")
+      .attr("class", "fa fa-times")
+      .attr('href', course => {
+        const url = scriptDataset.blockRemoveCourseUrl + "?slug=" + course.slug
+          + "&private=" + course.is_priv;
+        return url;
     });
 
-    /* Buttons to remove a course */
-    var removeButton = courses
-      .append("p").attr("class", "btn btn-link delete-course block-remove-button")
-      .append("a").attr("class", "fa fa-times")
-      .attr('href', function(d) {
-        return scriptDataset.blockRemoveCourseUrl + "?slug=" + d.slug + "&private=" + d.is_priv;
+    // Clicking the button removes the course.
+    $(".courseoccasion-info").each(function () {
+      $(this).modalForm({
+        formURL: $(this).data('id')
+      });
     });
   }
+}
 
-  /* Click functionality for adding courses */
-  $(".add-course").each(function () {
-    $(this).modalForm({formURL: $(this).data('id')});
-  });
+/**
+ * Adds a footer to any newly created academic years. The footer contains
+ * buttons to add courses to any of the four periods, but only if the user is
+ * logged in.
+ * 
+ * @param {*} academicYear - D3 data structure.
+ * @param {boolean} isLoggedIn - True if the user is logged in.
+ */
+function updateAcademicYearFooter(academicYear, isLoggedIn) {
+  // Add a footer to any newly created academic years.
+  let footer = academicYear
+    .append("div")
+    .attr("class", "academic-year-footer");
 
-  $(".courseoccasion-info").each(function () {
-    $(this).modalForm({formURL: $(this).data('id')});
-  });
+  /* Add divs for footer buttons "add course" */
+  // Create a selection of buttons for each period, including those that need
+  // to be created, based on the data attached.
+  let footerPeriodSelection = footer.selectAll(".academic-year-footer-period")
+    .data(courseGroup => [
+      { periodNumber: 1, year: courseGroup.year },
+      { periodNumber: 2, year: courseGroup.year },
+      { periodNumber: 3, year: courseGroup.year },
+      { periodNumber: 4, year: courseGroup.year }
+    ]);
+
+  // Only logged in user can add courses.
+  if (isLoggedIn) {
+    // Add, if needed, headers to the academic years that shows the four periods.
+    let footerPeriod = footerPeriodSelection
+      .enter()
+      .append("div")
+      .attr("class", "academic-year-footer-period bg-dark btn add-course")
+      .text("Lägg till kurs")
+      .style("color", "white");
+    
+    // Setup data for the pop-up modal.
+    // XXX: The page is refreshed each time a course is added. Could this be
+    // solved by instead sending a POST request?
+    footerPeriod
+      .attr("data-toggle", "tooltip")
+      .attr("data-placement", "left")
+      .attr('data-id', period => {
+        // XXX: Why use the start weeks 0, 10, 20 and 30 when the period number
+        // would do? The code would be clearer if the server did the week
+        // converersion instead.
+        const start = (period.periodNumber - 1) * 10;
+        const url = scriptDataset.blockCourseListUrl + "?year=" + period.year
+          + "&start=" + start;
+        return url;
+    });
+
+    // Show a pop-up modal with a list of courses to add when the user clicks
+    // on a button.
+    $(".add-course").each(function () {
+      $(this).modalForm({
+        formURL: $(this).data('id')
+      });
+    });
+
+  // Otherwise just add a filler spanning the full width.
+  } else {
+    footer
+      .append("div")
+      .attr("class", "academic-year-footer-year bg-dark");
+  }
+}
+
+/**
+  * Renders block schedule from the global course data. This function should be
+  * called every time data is changed, for example when adding/removing an
+  * academic year or course. The DOM elements are then
+  * updated accordingly.
+  */
+function renderBlockSchedule() {
+  const isLoggedIn = scriptDataset.isLoggedIn === 'True';
+
+  const transitionDuration = 500;
+
+  const xMax = 40;
+  const margin = 1;
+  const scale = 3;
+
+  // Make sure the groups of courses are sorted by year in ascending order.
+  coursesByYear.sort((a, b) => a.year - b.year);
+
+  // Container for all the academic years. Each year has headers, a set of
+  // courses and a footer.
+  const academicYearContainer = d3.select('#academic-year-container');
+
+  // Update all DOM elements. Changes can be because an academic year or course
+  // has been added or removed.
+  let academicYear = updateAcademicYear(academicYearContainer, transitionDuration);
+  updateTermsHeader(academicYear);
+  updatePeriodsHeader(academicYear);
+  updateCourseBlocks(academicYear, isLoggedIn, xMax, scale, margin);
+  updateAcademicYearFooter(academicYear, isLoggedIn);
 }
 
 /**
@@ -391,7 +488,7 @@ function block_interface_main() {
 
   createCategorySumChart();
 
-  renderBlock();
+  renderBlockSchedule();
     
   setupAddYearButton();
 
