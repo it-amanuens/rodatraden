70ec176073a0b26be313baf0a5639da6276354e9
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-06-23 18:59:57 +0200

    Put block-schedule scripts in directory
    I've put all JavaScript related to the block-schedule in a separate
    directory. To make that possible I've moved the functionality specific
    to the page but not the schedule in a separate file.

--- rodatraden/static/rodatraden/block_detail.js
+++ rodatraden/static/rodatraden/block_detail.js
@@ -1,30 +1,3 @@
-import BlockScheduleData from './block_schedule_data.js';
-import updateBlockSchedule from './update_block_schedule.js'
-
-// Variables from data parameters in a script tag. These can be made global
-// (using "window.") so that they can be used in other scripts.
-const dataElement = document.getElementById('string-data');
-const startYear = parseInt(dataElement.dataset.startYear);
-window.isLoggedIn = dataElement.dataset.isLoggedIn === 'True';
-window.courseoccasionInfoUrl = dataElement.dataset.courseoccasionInfoUrl;
-window.blockRemoveCourseUrl = dataElement.dataset.blockRemoveCourseUrl;
-window.blockCourseListUrl = dataElement.dataset.blockCourseListUrl;
-
-// Data that whose lifetime has to persist due to event listeners.
-let coursesData = new BlockScheduleData(startYear);
-let shouldStackTerms = isNarrowWindow();
-
-/**
- * Determines if the window is narrow or not.
- * 
- * @returns True if the window is too narrow.
- */
-function isNarrowWindow() {
-  // TEMP: Arbitrarily chosen pixel value.
-  const windowWidthThreshold = 800;
-  return window.innerWidth < windowWidthThreshold;
-}
-
 /**
  * Setups event listeners for the buttons to update and delete the block-
  * schedule.
@@ -67,19 +40,6 @@ function setupSections() {
     $("#block-schedule").hide("fast");
     $("#chart").hide("fast");
     $("#ISP").show("slow");
-  });
-}
-
-/**
- * Setups the button that adds a year to the block schedule by adding an
- * onclick eventlistener. Clicking on it adds an additional year to the
- * schedule.
- */
-function setupAddYearButton() {
-  const button = document.getElementById('block-addyear');
-  button.addEventListener('click', () => {
-    coursesData.addAcademicYear();
-    updateBlockSchedule(coursesData.academicYears, shouldStackTerms);
   });
 }
 
@@ -155,23 +115,16 @@ function createCategorySumChart() {
 }
 
 /**
- * Main function for this file.
+ * Main function for this script.
  */
-function block_interface_main() {
-  // Initialize the course data for the first time.
-  coursesData.update(shouldStackTerms);
-
+function main() {
   // Setup the three sections and most buttons.
   setupUpdateAndDeleteButtons();
   setupSections();
-  setupAddYearButton();
-
+  
   // Create the chart showing ECTS sums for each category.
   createCategorySumChart();
-
-  // Create the block-schedule.
-  updateBlockSchedule(coursesData.academicYears, shouldStackTerms);  
-
+  
   // Show the name of the file to be uploaded when a new file has been selected.
   document.querySelector('.custom-file-input').addEventListener(
     'change',
@@ -181,18 +134,7 @@ function block_interface_main() {
       label.innerText = fileName;
     }
   );
-
-  // When the window resizes from narrow to wide or vice versa, update the data
-  // and redraw the block-schdule.
-  window.addEventListener('resize', () => {
-    const oldTermLayout = shouldStackTerms;
-    shouldStackTerms = isNarrowWindow();
-    if (shouldStackTerms !== oldTermLayout) {
-      coursesData.update(shouldStackTerms);
-      updateBlockSchedule(coursesData.academicYears, shouldStackTerms);
-    }
-  })
 }
 
 // Run main function when the script is loaded.
-block_interface_main();
+main();
