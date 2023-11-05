0e9cb7b665c037b728a3c200bbd5db4912b3f658
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-11-05 23:17:37 +0100

    Implement remaining buttons
    The course update modal now has all buttons for prerequisites working,
    except in the case when there are no prerequisites at all. It is
    currently not possible to add the first prerequisite from nothing.

--- rodatraden/static/rodatraden/course_form.js
+++ rodatraden/static/rodatraden/course_form.js
@@ -47,24 +47,74 @@ function addSeperatorBetweenPrerequisites() {
 }
 
 /**
+ * Determines if it is suitable to add an equivalent course. This is the case if
+ * the last equivalent course has been selected. Otherwise, the user should
+ * select a course before adding a new one.
+ * 
+ * @param {HTMLElement} prerequisite The prerequisite of interest.
+ * @returns True if a new equivalent course should be added.
+ */
+function shouldAddEquivelentCourse(prerequisite) {
+  const lastSelect = prerequisite.querySelector('select:last-of-type');
+  const value = lastSelect.value;
+  return value !== '';
+}
+
+/**
  * Determines if it it suitable to add a new prerequisite. This is the case if
- * the last prerequisite has been filled in. Otherwise, the user should complete
- * the last prerequisite before adding a new one.
+ * the last prerequisite has at least one course selected. Otherwise, the user
+ * should complete the last prerequisite before adding a new one.
  * 
  * @returns True if a new prerequisite should be added.
  */
 function shouldAddPrerequisite() {
-  // Get the last prerequisite.
-  const lastPrerequisite = document.querySelector('.form-prerequisite:last-of-type')
+  const lastPrerequisite = document.querySelector(
+    '.form-prerequisite:last-of-type'
+  )
 
-  // Get the value of the first select.
+  // If there are no prerequisites, it is suitable to add a new one.
+  if (!lastPrerequisite) return true;
+
+  // Get the value of the first select, since it is guaranteed to exist.
   const select = lastPrerequisite.querySelector('select');
   const value = select.value;
   
   // If the value is empty, the prerequisite is incomplete. The last
   // prerequisite should be completed before a new one is added.
-  const isIncomplete = value === '';
-  return !isIncomplete;
+  return value !== '';
+}
+
+function addEquivalentCourse() {
+  // Since the button is a child of the prerequisite, the parent is the
+  // prerequisite.
+  const prerequisite = this.parentNode;
+
+  if (!shouldAddEquivelentCourse(prerequisite)) return;
+
+  // Get the current number of equivalent courses as well as the first course.
+  const courses = prerequisite.querySelectorAll('select');
+  const courseCount = courses.length;
+  const firstCourse = courses[0];
+
+  // Duplicate the first select and clear the value.
+  const newCourse = firstCourse.cloneNode(true);
+  newCourse.value = '';
+
+  // Change the name and id of the new select to be unique.
+  const newCourseName = newCourse.getAttribute('name');
+  const newCourseId = newCourse.getAttribute('id');
+  newCourse.setAttribute('name', newCourseName.replace('0', courseCount));
+  newCourse.setAttribute('id', newCourseId.replace('0', courseCount));
+
+  // Add a separator and the new course before the buttons.
+  const template = document.getElementById('prerequisite-or-template');
+  const separator = template.content.cloneNode(true);
+  const firstButton = prerequisite.querySelector('button');
+  prerequisite.insertBefore(separator, firstButton);
+  prerequisite.insertBefore(newCourse, firstButton);
+
+  // Adjust the CSS grid.
+  prerequisite.style.setProperty('--course-count', courseCount + 1);
 }
 
 /**
@@ -101,7 +151,9 @@ function addPrerequisite() {
   const firstAddButton = firstPrerequisite.querySelector('.add-equivalent-course');
   const firstRemoveButton = firstPrerequisite.querySelector('.remove-prerequisite');
   const newAddButton = firstAddButton.cloneNode(true);
+  newAddButton.addEventListener('click', addEquivalentCourse);
   const newRemoveButton = firstRemoveButton.cloneNode(true);
+  newRemoveButton.addEventListener('click', removePrerequisite);
   newPrerequisite.appendChild(newAddButton);
   newPrerequisite.appendChild(newRemoveButton);
 
@@ -113,10 +165,60 @@ function addPrerequisite() {
   prerequisiteContainer.appendChild(newPrerequisite);
 }
 
+function removeLeftoverSeparators() {
+  const separatorClassName = 'form-prerequisite__and-separator';
+
+  // Remove separators at the beginning and end of the list.
+  const prerequisiteContainer = document.getElementById('prerequisites');
+  const firstSeparator = prerequisiteContainer.querySelector(
+    `.${separatorClassName}:first-child`
+  );
+  const lastSeparator = prerequisiteContainer.querySelector(
+    `.${separatorClassName}:last-child`
+  );
+  console.log('firstSeparator', firstSeparator)
+  console.log('lastSeparator', lastSeparator)
+  if (firstSeparator) firstSeparator.remove();
+  if (lastSeparator) lastSeparator.remove();
+
+  // Remove adjacent separators.
+  const separators = prerequisiteContainer.querySelectorAll(
+    `.${separatorClassName} + .${separatorClassName}`
+  );
+  console.log('separators', separators)
+  for (const separator of separators) {
+    separator.remove();
+  }
+}
+
+function removePrerequisite() {
+  // Since the button is a child of the prerequisite, the parent is the
+  // prerequisite.
+  const prerequisite = this.parentNode;
+
+  // Remove the prerequisite and any leftover invalid separator.
+  prerequisite.remove();
+  removeLeftoverSeparators();
+}
+
 function main() {
   adjustPrerequisiteGrid();
   addSeperatorWithinPrerequisites();
   addSeperatorBetweenPrerequisites();
+
+  const addEquivalentCourseButtons = document.querySelectorAll(
+    '.add-equivalent-course'
+  );
+  for (const button of addEquivalentCourseButtons) {
+    button.addEventListener('click', addEquivalentCourse);
+  }
+
+  const removePrerequisiteButtons = document.querySelectorAll(
+    '.remove-prerequisite'
+  );
+  for (const button of removePrerequisiteButtons) {
+    button.addEventListener('click', removePrerequisite);
+  }
 
   const addPrerequisiteButton = document.getElementById('add-prerequisite');
   addPrerequisiteButton.addEventListener('click', addPrerequisite);
