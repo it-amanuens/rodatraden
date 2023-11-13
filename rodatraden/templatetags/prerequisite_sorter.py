411cac9734e34acb34d508585d135453170fc463
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-11-13 17:11:31 +0100

    Sort the new prerequisites
    Now the new prerequisites appear more ordered in the course detail view.
    The sorting logic is explained in the new sorting function.

---
+++ rodatraden/templatetags/prerequisite_sorter.py
@@ -0,0 +1,30 @@
+from django import template
+from django.db.models import Manager
+
+from rodatraden.models import PrerequisiteNew
+
+register = template.Library()
+
+@register.filter
+def sort_prerequisites(prerequisites: Manager[PrerequisiteNew]):
+  """Since a prerequisite can have multiple equivalent courses. There is no
+  obvious sort order. If we assume the equivalent courses will be displayed
+  ordered by title, then we can sort the prerequisites by the title of the first
+  equivalent course that would appear in that list.
+  
+  We therefore sort the prerequisites by the title of the equivalent course that
+  would appear first in the alphabet.
+  """
+
+  # This will contain tuples of (sort_key, prerequisite) and be used when
+  # sorting the prerequisites.
+  prerequisites_with_sort_key = []
+
+  for prerequisite in prerequisites:
+    # Here we assume that at least one equivalent course exists.
+    key: str = prerequisite.equivalent_prerequisites.order_by('title')[0].title
+    prerequisites_with_sort_key.append((key, prerequisite))
+  
+  # Sort by the key and return just the prerequisites.
+  prerequisites_with_sort_key.sort(key=lambda x: x[0])
+  return [prerequisite for _, prerequisite in prerequisites_with_sort_key]
